ec2f9c02a68e5d028abf231d0f19f70eee106f98
Lidiya Nikolova <lemi.dev@gmail.com>
2016-11-04 13:45:09 +0000

    Create p9.js

---
+++ p9.js
@@ -0,0 +1,22 @@
+//There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+//Find the product abc.
+//ANSWER: 375 * 200 * 425 = 31875000.
+
+//NOTE: n and m are <= 500 because:
+// Euclid's Formula on generating Pythagorean Triplets: a = m^2 – n^2, b = 2mn, c = m^2 + n^2
+// m^2 - n^2 + 2mn + m^2 + n^2 = 1000 
+// m(m+n) = 500
+
+function abc() {
+  for (var n = 1; n <= 500; n++) {
+    for (var m = 1; m <= 500; m++) {
+      var a = (m * m) - (n * n),
+          b = 2 * m * n,
+          c = (m * m) + (n * n);
+      if (a + b + c == 1000) {
+        return a * b * c;
+      }
+    }
+  } 
+}
+console.log(abc());
